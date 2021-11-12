cebf7091b315add24c75a7d609a33ccad0ea1d9d
Saleha <salehaahmed456@gmail.com>
2021-11-11 23:01:35 -0500

    metadata filter working for test sample

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,27 @@
-// d3.json("samples.json", function(data) {
-//     console.log(data);
-// });
+
+////// Needs to be populated:///////////////
+// id="selDataset" samples to populate dropdown
+// id="sample-metadata" demographic
+// id="bar" horizontal bar chart
+// id="gauge" gauge
+// id="bubble"  bubble chart
 
 
-d3.json("samples.json").then(function(data){ console.log(data)});
+// Read in JSON data
+
+
+
+
+// d3.json("samples.json").then(function(data){ console.log(data)});
+
+
+
+function getMetadata(sample) {
+    d3.json("samples.json").then((data) => {
+        let metadata = data.metadata.filter(function(meta) { return meta.id == sample; })[0];
+        console.log(metadata)
+    });
+
+};
+
+console.log(getMetadata(940))
